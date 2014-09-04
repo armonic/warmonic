b536dfc67496d2723e5d132f7e31932b984894f4
Jean-Philippe Braun <eon@patapon.info>
2014-09-04 16:00:24 +0200

    Remember login values

--- app/js/login.js
+++ app/js/login.js
@@ -2,23 +2,18 @@
 
 angular.module('warmonic.login', [])
 
-.controller('loginCtrl', ['$scope', '$state', 'xmppSession', 'xmpp', function($scope, $state, xmppSession, xmpp) {
+.controller('loginCtrl', ['$scope', '$cookieStore', '$state', 'xmppSession', 'xmpp', function($scope, $cookieStore, $state, xmppSession, xmpp) {
 
-  this.masterJID = null;
-  this.mucDomain = null;
-  this.domain = null;
-  this.connection = null;
+  this.info = $cookieStore.get('warmonicLogin') || {jid: null, mucDomain: null, connection: null};
+  this.password = null;
+  this.commandsProvider = null;
   this.connections = [];
-  this.user = {
-    'jid': 'master@im.aeolus.org',
-    'password': 'master'
-  };
 
   $scope.$watch(
-    angular.bind(this, function() { return this.user.jid; }),
+    angular.bind(this, function() { return this.info.jid; }),
     angular.bind(this, function(newVal, oldVal) {
       try {
-        this.domain = Strophe.getDomainFromJid(this.user.jid);
+        this.domain = Strophe.getDomainFromJid(this.info.jid);
       }
       catch (error) {
         this.domain = "";
@@ -28,8 +23,19 @@ angular.module('warmonic.login', [])
         {label: "BOSH connection", value: 'http://' + this.domain + ':5280/http-bind'}
       ];
       this.connection = this.connections[0].value;
-      this.masterJID = this.user.jid + '/master';
-      this.mucDomain = "logs." + this.domain;
+      if (this.info.connection) {
+        for (var i=0; i<this.connections.length; i++) {
+          if (this.connections[i].value == this.info.connection) {
+            this.connection = this.info.connection;
+            break;
+          }
+        }
+      }
+      this.commandsProvider = this.info.jid + '/master';
+      if (this.info.mucDomain && this.domain && this.info.mucDomain.indexOf(this.domain) != -1)
+        this.mucDomain = this.info.mucDomain;
+      else
+        this.mucDomain = "logs." + this.domain;
   }));
 
   this.status = xmpp.status;
@@ -37,7 +43,10 @@ angular.module('warmonic.login', [])
   this.connect = function() {
     xmppSession.data.commandsProvider = this.masterJID;
     xmppSession.data.mucDomain = this.mucDomain;
-    xmpp.connect(this.user.jid, this.user.password, this.connection);
+    xmpp.connect(this.info.jid, this.password, this.connection);
+    $cookieStore.put('warmonicLogin', {jid: this.info.jid,
+                                       mucDomain: this.mucDomain,
+                                       connection: this.connection});
   };
 
   this.disconnect = function() {
