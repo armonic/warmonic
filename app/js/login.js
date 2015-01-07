4b88a882b6002532c2082f2cc8b49d4077d4d59f
lewo <aeiche@mandriva.com>
2015-01-07 17:35:16 +0100

    login: add field 'host' to optionnaly specify the XMPP host adress.
    This is useful when the XMPP server can not be reached with the XMPP domain.

--- app/js/login.js
+++ app/js/login.js
@@ -7,13 +7,14 @@ angular.module('warmonic.login', [])
   if (xmpp.connected)
       $state.go('provides');
 
-  this.info = $cookieStore.get('warmonicLogin') || {jid: null, mucDomain: null, connection: null};
+  this.info = $cookieStore.get('warmonicLogin') || {jid: null, mucDomain: null, connection: null, host: null};
   this.password = null;
   this.commandsProvider = null;
   this.connections = [];
+  this.host = null;
 
   $scope.$watch(
-    angular.bind(this, function() { return this.info.jid; }),
+    angular.bind(this, function() { return (this.info.jid, this.host); }),
     angular.bind(this, function(newVal, oldVal) {
       try {
         this.domain = Strophe.getDomainFromJid(this.info.jid);
@@ -21,9 +22,15 @@ angular.module('warmonic.login', [])
       catch (error) {
         this.domain = "";
       }
+
+      // By default, the XMPP domain is used
+      if (this.host === null) {
+	this.host = this.domain;
+      }
+
       this.connections = [
-        {label: "Websocket connection", value: 'ws://' + this.domain + ':5280/xmpp-websocket'},
-        {label: "BOSH connection", value: 'http://' + this.domain + ':5280/http-bind'}
+        {label: "Websocket connection", value: 'ws://' + this.host + ':5280/xmpp-websocket'},
+        {label: "BOSH connection", value: 'http://' + this.host + ':5280/http-bind'}
       ];
       this.connection = this.connections[0].value;
       if (this.info.connection) {
@@ -51,7 +58,8 @@ angular.module('warmonic.login', [])
     xmpp.connect(this.info.jid, this.password, this.connection);
     $cookieStore.put('warmonicLogin', {jid: this.info.jid,
                                        mucDomain: this.mucDomain,
-                                       connection: this.connection});
+                                       connection: this.connection,
+				       host: this.host});
   };
 
   this.disconnect = function() {
